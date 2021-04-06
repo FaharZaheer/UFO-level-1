92faef7471eeb09eba4425bd265e63d5f6cb02ec
muhammad fahar <faharzaheer@hotmail.com>
2021-04-05 22:57:40 -0400

    commit with data and filter button

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var filtertable = d3.select(".filtertable");
+
+// Use D3 `.on` to attach a click handler for the filtertable
+filtertable.on("click", function() {
+
+console.log("filter button pushed")
+
+});
