8b4fb1a1ea65a72338280a5e1b8324e62c25977e
muhammad fahar <faharzaheer@hotmail.com>
2021-04-05 23:39:42 -0400

    commit with filter function and display filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,28 @@ var tableData = data;
 // YOUR CODE HERE!
 var filtertable = d3.select(".filtertable");
 
+var form = d3.select("#form");
+
+// Create event handlers 
+
 // Use D3 `.on` to attach a click handler for the filtertable
 filtertable.on("click", function() {
 
 console.log("filter button pushed")
-
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+console.log(inputValue)
+var tbody = d3.select("tbody");
+var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+console.log(filteredData)
+tbody.html("");
+//
+filteredData.forEach((ufodata) => {
+  var row = tbody.append("tr");
+  Object.entries(ufodata).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+//
 });
